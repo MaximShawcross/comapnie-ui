186a1e68d81864eadf35a75bdd64c1377c9eccca
Kushnir Maxim <maxbasta2006@gmail.com>
2023-02-01 16:28:27 +0100

    router-dom package and base routing

--- src/app/app.js
+++ src/app/app.js
@@ -1,13 +1,25 @@
 import { Sidebar } from "../components/sidebar/sidebar";
 import { HomePage, LoginPage, CompaniesPage, SignInPage } from '../pages/index';
+import { createBrowserRouter } from 'react-router-dom';
 import "./app.scss";
 
-export const App = () => {
+export const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <HomePage />
+	},
+	{
+		path: "/login",
+		element: <LoginPage />
+	},
+	{
+		path: "/companies",
+		element: <CompaniesPage />
+	},
+	{
+		path: "/register",
+		element: <SignInPage />
+	},
+]);
 
-	return (
-		// <HomePage/>
-		// <LoginPage/>
-		// <CompaniesPage />
-		<SignInPage/>
-	)
-}
+
--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
-import { App } from './app/app';
+import { App, router } from './app/app';
 import './styles/index.scss';
+import {  RouterProvider } from 'react-router-dom';
+
 
 const container = document.getElementById('root');
 const root = createRoot(container);
@@ -11,7 +13,7 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+		<RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
